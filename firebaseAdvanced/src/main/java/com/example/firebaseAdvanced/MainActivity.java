f88bf023887c3e65012d871739b6761122b58296
Linh Trần <trung28899@gmail.com>
2019-11-25 15:19:46 -0500

    Failed Update Button

--- firebaseAdvanced/src/main/java/com/example/firebaseAdvanced/MainActivity.java
+++ firebaseAdvanced/src/main/java/com/example/firebaseAdvanced/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.firebaseAdvanced;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +29,7 @@ import java.util.List;
 
 // Adding Artist and Showing List of Artists, retrieved from the database
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     public static final String ARTIST_NAME = "artistname";
     public static final String ARTIST_ID = "artistId";
 
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     List<Artist> listOfArtists;
 
 
+    // Function for declarations, Buttons and ListView Items listeners
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,23 +58,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         listOfArtists = new ArrayList<>();
 
-        buttonAdd.setOnClickListener(this);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 addArtist();
+             }
+        });
 
-        // Item Click Listener for Items in ListView
-        listViewArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                // Item Click Listener for Items in ListView
+                listViewArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Artist artist = listOfArtists.get(position);
+                        Intent intent = new Intent(getApplicationContext(), AddTrackActivity.class);
+
+                        intent.putExtra(ARTIST_ID, artist.getArtistId());
+                        intent.putExtra(ARTIST_NAME, artist.getArtistName());
+
+                        startActivity(intent);
+                    }
+                });
+
+        // Item in ListView listener for Long Click (Click and Hold)
+        listViewArtists.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Artist artist = listOfArtists.get(position);
-                Intent intent = new Intent(getApplicationContext(), AddTrackActivity.class);
 
-                intent.putExtra(ARTIST_ID, artist.getArtistId());
-                intent.putExtra(ARTIST_NAME, artist.getArtistName());
+                showUpdateDialog(artist.getArtistId(), artist.getArtistName());
 
-                startActivity(intent);
+                return false;
             }
         });
     }
 
+    // Function to Display functionally the Diaglog for updating fields
+    private void showUpdateDialog(final String artistId, String artistName){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+        final Button buttonUpdate = (Button) findViewById(R.id.update);
+        final Spinner spinnerGenres1 = (Spinner) findViewById(R.id.spinner);
+
+        dialogBuilder.setTitle("Updating Artist: " + artistName);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                 String name = editTextName.getText().toString().trim();
+                String genre = spinnerGenres1.getSelectedItem().toString();
+
+                if(TextUtils.isEmpty(name)){
+                    editTextName.setError("Name Required");
+                    return;
+                }
+                updateArtist(artistId, name, genre);
+                // close dialog after update
+                alertDialog.dismiss();
+                */
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // Function for Adding Artist
     private void addArtist(){
         String name = editTextName.getText().toString().trim();
         String genres = spinnerGenres.getSelectedItem().toString();
@@ -94,6 +155,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    // Function for updating data from database
     @Override
     protected void onStart() {
         super.onStart();
@@ -127,10 +189,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view == buttonAdd){
-            addArtist();
-        }
+    // Function for updating Artist information in the
+    private boolean updateArtist(String id, String name, String genre){
+        // Getting the particular artist that need to be updated
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("artists").child(id);
+        // create a new artist with new id, name and genre
+        Artist artist = new Artist(id, name, genre);
+
+        // Over-writting the new artist into a specified id
+        databaseReference.setValue(artist);
+
+        Toast.makeText(this, "Artist is Updated Successfully", Toast.LENGTH_LONG).show();
+
+        return true;
     }
+
 }
